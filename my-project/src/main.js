7acc9cc2bb4961a029f28714b12b83939c294b2e
RK-sonbdt <dangson2376@gmail.com>
2026-01-28 15:57:22 +0700

    Set up tthe project

---
+++ my-project/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+import router from './routes/indnex'
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(Antd).use(router).mount('#app')
---
+++ my-project/src/routes/indnex.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../pages/admin/Dashboard.vue";
+
+const routes = [
+    {
+        path:"/",
+        name:"Dashboard",
+        component:Dashboard
+
+
+    }
+    
+]
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+})
+export default router
